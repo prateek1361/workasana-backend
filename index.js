aef306641bfc1ebad701a8d4c185f930c0e81c53
Prateek Sharma <prateek20091234@gmail.com>
2025-11-24 02:39:31 +0530

    feat:patch added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
